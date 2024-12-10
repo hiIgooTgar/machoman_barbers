24970d00eb5d96fbc1fdf7904d8286db68b0f47f
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-12-10 20:34:16 +0700

    Update Galeri Code | Machoman Barbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,30 @@ document.querySelectorAll(".nav-link").forEach((link) => {
     link.classList.add("active-menu");
   }
 });
+
+// pop up galeri
+function showPopUpGaleri(event) {
+  const galeriImage = event.currentTarget
+    .querySelector("#galeri-img")
+    .getAttribute("src");
+
+  const overlayGaleri = document.createElement("div");
+  overlayGaleri.id = "overlay-galeri";
+
+  const contentGaleri = document.createElement("div");
+  contentGaleri.id = "content-galeri";
+  contentGaleri.innerHTML = `
+   <div>
+        <div class='float-right'>
+          <button class='bg-primary text-gray-950' id='popup-close'><i class='fa fa-x'></i></button>
+        </div>
+        <img src='${galeriImage}' alt='${galeriImage}' />
+    </div>`;
+
+  overlayGaleri.appendChild(contentGaleri);
+  document.body.appendChild(overlayGaleri);
+
+  document.getElementById("popup-close").addEventListener("click", () => {
+    document.body.removeChild(overlayGaleri);
+  });
+}
